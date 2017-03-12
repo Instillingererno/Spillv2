feb38c7ff11b59a8e1fe736f78d7127ca07549a7
Øyvind <yvind@DESKTOP-PB0UML9>
2017-03-12 01:59:27 +0100

    Setting up xp-dependant levels

--- wwwroot/script/Main_game_function.js
+++ wwwroot/script/Main_game_function.js
@@ -1,14 +1,15 @@
 ﻿var playerHealth = 100;
 var playerLevel = 1;
-var playerXp;
+var playerXp = 500;
 var invOpen = true;
 var queOpen = true;
 var menOpen = false;
 
-/*Setting player health equal to playerHealth value and the appropriate level*/
+/*Setting player health equal to playerHealth value and the appropriate level and xp*/
 function body_onload() {
     document.getElementById("healthbar").value = playerHealth;
-    document.getElementById("level-txt").innerHTML += "Level: " + playerLevel;
+    document.getElementById("level-txt").innerHTML = "Level: " + playerLevel;
+    document.getElementById("playerxp").value = playerXp;
 }
 
 /*Decrease player health*/
@@ -23,17 +24,6 @@ function btnKill() {
     playerHealth = 0;
     document.getElementById("healthbar").value = playerHealth;
     playerDeath();
-}
-
-/*Level up the player*/
-function levelUp() {
-    if (playerLevel >= 100) {
-        return false;
-    }
-    else {
-        playerLevel++;
-        document.getElementById("level-txt").innerHTML = "Level: " + playerLevel;
-    }
 }
 
 /*Check for player death*/
@@ -109,3 +99,29 @@ window.onkeyup = function (event) {
         menu_icon();
     }
 };
+
+
+
+
+
+
+/*WIP player xp*/
+function playerXP() {
+    playerXp = document.getElementById("playerxp").value;
+    playerXp += 100;
+    document.getElementById("playerxp").value = playerXp;
+    levelUp();
+}
+
+/*WIP player level*/
+function levelUp() {
+    if (playerLevel >= 100) {
+        return false;
+    }
+    if (playerXp == 1000) {
+        playerLevel++;
+        document.getElementById("level-txt").innerHTML = "Level: " + playerLevel;
+        playerXp -= 1000;
+        document.getElementById("playerxp").value = playerXp;
+    }
+}
